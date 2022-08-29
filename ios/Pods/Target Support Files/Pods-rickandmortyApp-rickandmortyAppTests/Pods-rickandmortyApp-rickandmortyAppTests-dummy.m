046886fb309857a07acefdaeac7aaad516f2cbfe
@marcos020499 <marcostorres.live@hotmail.com>
2022-08-29 17:33:05 -0500

    init App

---
+++ ios/Pods/Target Support Files/Pods-rickandmortyApp-rickandmortyAppTests/Pods-rickandmortyApp-rickandmortyAppTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_rickandmortyApp_rickandmortyAppTests : NSObject
+@end
+@implementation PodsDummy_Pods_rickandmortyApp_rickandmortyAppTests
+@end
